7a2b9c1080a9945f0fd95239988450932c7aca3b
Bryan Larsen <bryan@larsen.st>
2017-12-31 22:23:29 -0500

    First version complete

---
+++ Assets/Scripts/BuildBricks.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildBricks : MonoBehaviour {
+public Transform brick;
+	// Use this for initialization
+	void Start () {
+		for (float y = 0f; y < 5f; y++) {
+        	for (float x = -98; x < 100; x++) {
+				for (float z = -75; z < 99; z += 50) {
+            		Instantiate(brick, new Vector3(y%2==0 ? x : x+0.5f, y+0.5f, z), Quaternion.identity);
+    	    	}
+			}
+    	}
+ 
+		for (float y = 0f; y < 5; y++) {
+        	for (float x = -75; x < 100; x+=50) {
+				for (float z = -98; z < 100; z ++) {
+					if (y % 2 == 0) {
+						if ((z+25) % 50 != 0) {
+        	    	  		Instantiate(brick, new Vector3(x, y+0.5f, z), Quaternion.identity);
+        				}
+					} else {
+						if (((z+25) % 50 != 49) && ((z+25) % 50 != 0)) {
+							Instantiate(brick, new Vector3(x, y+0.5f, z+0.5f), Quaternion.identity);
+						}
+					}
+				}
+			}
+    	}
+	}
+	
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public float rotateAngle;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private Transform tf;
+    private Animator anim;
+    private int animSpeedHash = Animator.StringToHash("Speed");
+    private int count;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        count = 0;
+        countText.text = "Get some hearts!";
+        winText.text = "";
+    }
+
+    void Update() {
+        if (Input.GetKey("escape")) {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            Application.Quit();
+        }
+    }
+
+    void FixedUpdate ()
+    {
+        float x = Input.GetAxis ("Horizontal");
+        float z = Input.GetAxis ("Vertical");
+        float r = Input.GetAxis ("Mouse X");
+
+        transform.Rotate(0, r, 0);
+        rb.AddForce (transform.forward * z * speed);
+        rb.AddForce (transform.right * x * speed);
+        //anim.speed = Math.Min(1.0f, rb.velocity.magnitude / 4);
+        Debug.Log(rb.velocity.magnitude);
+        anim.SetFloat(animSpeedHash, rb.velocity.magnitude * rotateAngle);
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Heart")) {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            countText.text = "Count: " + count.ToString();
+            if (count >= 2) {
+                winText.text = "You Win!";
+                anim.speed = 0.1f;
+                anim.SetTrigger("Win");
+            }
+        }
+    }
+}
